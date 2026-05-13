c55f7c591de47cae75735ce03d62094c8ecbfda1
Natanael Rio Wijaya <gafterlife@gmail.com>
2026-05-13 10:15:47 +0700

    rio

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
             'cloudinary.com'
         ], images: {
             unoptimized: true,
-        },
+        }
     }, async headers() {
         return [
             {
